43a25268f09f8c6f55de9f908e8eab0482f385cf
Earthcomputer <burtonjae@hotmail.co.uk>
2016-09-19 16:35:19 +0100

    Working better middle click

--- src/main/java/net/earthcomputer/redbuilder/BetterMiddleClickListener.java
+++ src/main/java/net/earthcomputer/redbuilder/BetterMiddleClickListener.java
@@ -1,17 +1,21 @@
 package net.earthcomputer.redbuilder;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagByte;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
@@ -19,11 +23,20 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerChangedDimensionEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.relauncher.Side;
 
 public class BetterMiddleClickListener {
+
+	private Map<BlockPos, IBlockState> pendingBlockPlacements = Maps.newHashMap();
+	private Map<BlockPos, Integer> timeLeftAtPos = Maps.newHashMap();
+
+	private static final int TIME_TO_WAIT = 10;
 
 	@SubscribeEvent
 	public void onMiddleClicked(ClientTickEvent e) {
@@ -82,8 +95,7 @@ public class BetterMiddleClickListener {
 	}
 
 	@SubscribeEvent
-	public void onBlockPlaced(PlayerInteractEvent.RightClickBlock e) {
-		System.out.println("Player interact event");
+	public void onBlockRightClicked(PlayerInteractEvent.RightClickBlock e) {
 		if (e.getSide() != Side.CLIENT) {
 			return;
 		}
@@ -95,19 +107,80 @@ public class BetterMiddleClickListener {
 
 		NBTTagCompound stackTag = stack.getTagCompound();
 		if (stackTag == null || !stackTag.hasKey("PickedBlock", Constants.NBT.TAG_STRING)
-				|| !stackTag.hasKey("StateData", Constants.NBT.TAG_INT)) {
+				|| !stackTag.hasKey("StateData", Constants.NBT.TAG_BYTE)) {
 			return;
 		}
 
-		String blockName = stackTag.getString("PickedBlock");
-		if (!Block.REGISTRY.containsKey(new ResourceLocation(blockName))) {
+		Block block = Block.getBlockFromName(stackTag.getString("PickedBlock"));
+		if (block == null) {
 			return;
 		}
-		int metadata = stackTag.getInteger("StateData");
-		BlockPos pos = e.getPos().offset(e.getFace());
-		
-		Minecraft.getMinecraft().thePlayer.sendChatMessage(
-				String.format("/setblock %d %d %d %s %d", pos.getX(), pos.getY(), pos.getZ(), blockName, metadata));
+		int metadata = stackTag.getByte("StateData");
+		// No-one will ever know why this is deprecated :/
+		@SuppressWarnings("deprecation")
+		IBlockState state = block.getStateFromMeta(metadata);
+		BlockPos pos = e.getPos();
+		if (!block.canReplace(e.getWorld(), pos, e.getFace(), stack)) {
+			pos = pos.offset(e.getFace());
+		}
+
+		pendingBlockPlacements.put(pos, state);
+		timeLeftAtPos.put(pos, TIME_TO_WAIT);
+	}
+
+	@SubscribeEvent
+	public void tick(TickEvent.PlayerTickEvent e) {
+		if (e.side != Side.CLIENT) {
+			return;
+		}
+
+		Map<BlockPos, Integer> newMap = Maps.newHashMap();
+		World world = e.player.worldObj;
+		for (Map.Entry<BlockPos, Integer> entry : timeLeftAtPos.entrySet()) {
+			BlockPos pos = entry.getKey();
+			int newValue = entry.getValue() - 1;
+			if (newValue == 0) {
+				IBlockState newState = pendingBlockPlacements.remove(pos);
+				if (world.getBlockState(pos).getBlock() == newState.getBlock()) {
+					Minecraft.getMinecraft().thePlayer.sendChatMessage(String.format("/setblock %d %d %d %s %d",
+							pos.getX(), pos.getY(), pos.getZ(), Block.REGISTRY.getNameForObject(newState.getBlock()),
+							newState.getBlock().getMetaFromState(newState)));
+				}
+			} else {
+				newMap.put(pos, newValue);
+			}
+		}
+		timeLeftAtPos.clear();
+		timeLeftAtPos.putAll(newMap);
+	}
+
+	@SubscribeEvent
+	public void onPlayerChangedDimension(PlayerChangedDimensionEvent e) {
+		if (e.player instanceof EntityPlayerMP) {
+			return;
+		}
+		clearPendingBlocks();
+	}
+
+	@SubscribeEvent
+	public void onPlayerLoggedOut(PlayerLoggedOutEvent e) {
+		if (e.player instanceof EntityPlayerMP) {
+			return;
+		}
+		clearPendingBlocks();
+	}
+
+	@SubscribeEvent
+	public void onPlayerRespawned(PlayerRespawnEvent e) {
+		if (e.player instanceof EntityPlayerMP) {
+			return;
+		}
+		clearPendingBlocks();
+	}
+
+	private void clearPendingBlocks() {
+		pendingBlockPlacements.clear();
+		timeLeftAtPos.clear();
 	}
 
 }
